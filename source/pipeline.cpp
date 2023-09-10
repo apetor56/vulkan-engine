50c8e0eb4bd2875c05317e7b3ad452881396020d
apetor56 <lukten.github@gmail.com>
2023-09-10 02:19:09 +0200

    createShaderModule()

--- source/pipeline.cpp
+++ source/pipeline.cpp
@@ -5,8 +5,11 @@
 
 namespace VE {
 
-Pipeline::Pipeline(std::string_view vertFilePath, std::string_view fragFilePath) {
-    createGraphicsPipeline(vertFilePath, fragFilePath);
+Pipeline::Pipeline(Device& device,
+                   std::string_view vertFilePath,
+                   std::string_view fragFilePath,
+                   const PipelineConfigInfo& configInfo) : m_device(device) {
+    createGraphicsPipeline(vertFilePath, fragFilePath, configInfo);
 }
 
 std::vector<char> Pipeline::readFile(std::string_view filePath) {
@@ -26,12 +29,31 @@ std::vector<char> Pipeline::readFile(std::string_view filePath) {
     return buffer;
 }
 
-void Pipeline::createGraphicsPipeline(std::string_view vertFilePath, std::string_view fragFilePath) {
-    const auto vertCode {std::move(readFile(vertFilePath))};
-    const auto fragCode {std::move(readFile(fragFilePath))};
+void Pipeline::createGraphicsPipeline(std::string_view vertFilePath,
+                                      std::string_view fragFilePath,
+                                      const PipelineConfigInfo& configInfo) {
+    const auto vertCode { std::move(readFile(vertFilePath)) };
+    const auto fragCode { std::move(readFile(fragFilePath)) };
 
     std::cout << "vertex shader binary code size: " << std::size(vertCode) << '\n'
               << "fragment shader binary code size: " << std::size(fragCode) << '\n';
+}
+
+void Pipeline::createShaderModule(const std::vector<char> code, VkShaderModule *shaderModule) {
+    VkShaderModuleCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+    createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+    createInfo.codeSize = std::size(code);
+
+    if(vkCreateShaderModule(m_device.getDevice(), &createInfo, nullptr, shaderModule) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create shader module");
+    }
+}
+
+PipelineConfigInfo Pipeline::defaultPipelineConfigInfo(uint32_t width, uint32_t height) {
+    PipelineConfigInfo configInfo{};
+
+    return configInfo;
 }
 
 }
