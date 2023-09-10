10334b3189697983674fe1836aecd3285b8a448d
apetor56 <lukten.github@gmail.com>
2023-09-10 02:15:52 +0200

    added gpu and device configs

--- include/config.hpp
+++ include/config.hpp
@@ -3,13 +3,21 @@
 namespace cfg {
 
 namespace window {
-    inline constexpr int width {800};
-    inline constexpr int height {600};
-} // namespace window
+    inline constexpr int width { 800 };
+    inline constexpr int height { 600 };
+} // namespace m_window
 
 namespace shader {
     inline const char* vertShaderBinary{"../shaders/simple.vert.spv"};
     inline const char* fragShaderBinary{"../shaders/simple.frag.spv"};
 } // namespace shader
+
+namespace gpu {
+    inline constexpr size_t discreteGpuValue { 1000u };
+}
+
+namespace device {
+    inline constexpr uint32_t queueFamiliesCount { 2u };
+}
 
 } // namespace cfg
