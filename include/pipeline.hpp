bae290465ac381e1d50d71081899813f3c312c44
apetor56 <lukten.github@gmail.com>
2023-09-10 02:17:19 +0200

    small changes

--- include/pipeline.hpp
+++ include/pipeline.hpp
@@ -1,17 +1,40 @@
 #pragma once
 
+#include "device.hpp"
 #include <string_view>
 #include <vector>
 
 namespace VE {
 
+struct PipelineConfigInfo {};
+
 class Pipeline {
 public:
-    Pipeline(std::string_view vertFilePath, std::string_view fragFilePath);
+    Pipeline(Device& device,
+             std::string_view vertFilePath,
+             std::string_view fragFilePath,
+             const PipelineConfigInfo& configInfo);
+
+    ~Pipeline() {}
+
+    Pipeline(const Pipeline& other) = delete;
+    Pipeline& operator=(const Pipeline other) = delete;
+
+    static PipelineConfigInfo defaultPipelineConfigInfo(uint32_t width, uint32_t height);
 
 private:
+    Device& m_device;
+    VkPipeline m_graphicsPipeline;
+    VkShaderModule m_vertShaderModule;
+    VkShaderModule m_fragShaderModule;
+
     static std::vector<char> readFile(std::string_view filePath);
-    void createGraphicsPipeline(std::string_view vertFilePath, std::string_view fragFilePath);
+
+    void createGraphicsPipeline(std::string_view vertFilePath,
+                                std::string_view fragFilePath,
+                                const PipelineConfigInfo& configInfo);
+
+    void createShaderModule(const std::vector<char> code, VkShaderModule *shaderModule);
 };
 
 };
