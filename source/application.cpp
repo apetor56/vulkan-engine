0f6ca6b1088ea388dd63ba72b4a640fbe8cc4776
apetor56 <lukten.github@gmail.com>
2023-10-22 00:34:21 +0200

    added new classes

--- source/application.cpp
+++ source/application.cpp
@@ -5,9 +5,12 @@
 
 namespace VE {
 
-Application::Application() : m_window{ std::make_shared<Window>(cfg::window::width, cfg::window::height, "example") },
-                             m_vulkanInstance { std::make_shared<VulkanInstance>() },
-                             m_device { m_vulkanInstance, m_window } {}
+Application::Application() : m_vulkanInstance { std::make_shared<VulkanInstance>() },
+                             m_window{ std::make_shared<Window>(cfg::window::width, cfg::window::height, "example", m_vulkanInstance) },
+                             m_physicalDevice { std::make_shared<PhysicalDevice>(m_vulkanInstance, m_window) },
+                             m_logicalDevice { std::make_shared<LogicalDevice>(m_physicalDevice, m_window) },
+                             m_swapchain { std::make_shared<Swapchain>(m_physicalDevice, m_logicalDevice, m_window) }
+                            {}
 
 void Application::run() {
     while(m_window->shouldClose() == false) {
