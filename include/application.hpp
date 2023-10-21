e0beec627603c7148d12efabe6d97033b255e542
Łukasz Tenerowicz <ltenerowicz@novomatic-tech.com>
2023-10-21 02:23:56 +0200

    added VulkanInstance shared ptr

--- include/application.hpp
+++ include/application.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "window.hpp"
+#include "vulkan_instance.hpp"
 #include "device.hpp"
 #include <memory>
 
@@ -14,6 +15,7 @@ public:
 
 private:
     std::shared_ptr<Window> m_window;
+    std::shared_ptr<VulkanInstance> m_vulkanInstance;
     Device m_device;
 };
 
