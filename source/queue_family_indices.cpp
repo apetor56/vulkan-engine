d7c51a96a0ff962e99253d1065c08831a3ad926f
apetor56 <lukten.github@gmail.com>
2023-10-22 00:34:04 +0200

    added queue family indices struct

---
+++ source/queue_family_indices.cpp
@@ -0,0 +1,44 @@
+#include "queue_family_indices.hpp"
+
+#include <vector>
+
+namespace VE {
+
+bool QueueFamilyIndices::isComplete() const {
+    return graphicsFamily.has_value() && presentFamily.has_value();
+}
+
+QueueFamilyIndices QueueFamilyIndices::findQueueFamilies(const VkPhysicalDevice device, const VkSurfaceKHR surface) {
+    QueueFamilyIndices queueFamilyIndices{};
+
+    uint32_t queueFamilyCount{};
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+    std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+    uint32_t queueFamilyIndex{};
+    VkBool32 isPresentSupportAvailable{ false };
+
+    for(const auto& queueFamily : queueFamilies) {
+        if(queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+            queueFamilyIndices.graphicsFamily = queueFamilyIndex;
+        }
+
+        vkGetPhysicalDeviceSurfaceSupportKHR(device, queueFamilyIndex, surface, &isPresentSupportAvailable);
+
+        if(isPresentSupportAvailable) {
+            queueFamilyIndices.presentFamily = queueFamilyIndex;
+        }
+
+        if(queueFamilyIndices.isComplete()) {
+            break;
+        }
+
+        queueFamilyIndex++;
+    }
+
+    return queueFamilyIndices;
+}
+
+}
