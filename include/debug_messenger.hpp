c05c4635ee21e05f56937e9f15c00da00daf8a71
Łukasz Tenerowicz <ltenerowicz@novomatic-tech.com>
2023-10-21 02:24:35 +0200

    class for debug messenger

---
+++ include/debug_messenger.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <vector>
+#include <memory>
+#include <vulkan/vulkan.h>
+
+namespace VE {
+
+class DebugMessenger {
+public:
+    void init(const VkInstance vulkanInstance);
+
+    void destroy(const VkInstance vulkanInstance);
+
+    bool areValidationLayersSupported() const;
+
+    const std::vector<const char*>& getLayers() const;
+
+    void populateCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo) const;
+
+private:
+    VkDebugUtilsMessengerEXT m_debugMessenger;
+    const std::vector<const char*> m_validationLayers { "VK_LAYER_KHRONOS_validation" };
+
+    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+                                                        VkDebugUtilsMessageTypeFlagsEXT messageType,
+                                                        const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+                                                        void* pUserData);
+
+    VkResult getDebugUtilsMessengerEXT(const VkInstance instance, 
+                                                       const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
+                                                       const VkAllocationCallbacks* pAllocator,
+                                                       VkDebugUtilsMessengerEXT* pDebugMessenger) const;
+
+    void destroyDebugUtilsMessengerEXT(const VkInstance instance,
+                                                       VkDebugUtilsMessengerEXT debugMessenger,
+                                                       const VkAllocationCallbacks* pAllocator) const;
+};
+
+}
