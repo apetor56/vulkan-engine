04b142235d6da0b7fe6013c6c64da83a6ebd7ff7
Łukasz Tenerowicz <ltenerowicz@novomatic-tech.com>
2023-10-21 02:22:42 +0200

    refactor

--- source/device.cpp
+++ source/device.cpp
@@ -13,9 +13,10 @@
 
 namespace VE {
 
-Device::Device(std::shared_ptr<Window> window) : m_physicalDevice(VK_NULL_HANDLE), m_window(window) {
-    createVulkanInstance();
-    setupDebugMessenger();
+Device::Device(std::shared_ptr<VulkanInstance> vulkanInstance,
+               std::shared_ptr<Window> window) : m_vulkanInstance { vulkanInstance },
+                                                 m_physicalDevice { VK_NULL_HANDLE }, 
+                                                 m_window { window } {
     createSurface();
     pickPhysicalDevice();
     createLogicalDevice();
@@ -24,10 +25,6 @@ Device::Device(std::shared_ptr<Window> window) : m_physicalDevice(VK_NULL_HANDLE
 }
 
 Device::~Device() {
-    if (m_isValidationEnabled) {
-        destroyDebugUtilsMessengerEXT(m_instance, m_debugMessenger, nullptr);
-    }
-
     vkDestroySwapchainKHR(m_device, m_swapchain, nullptr);
     
     std::ranges::for_each(m_swapChainImageViews, [this](const auto& view) {
@@ -35,164 +32,23 @@ Device::~Device() {
     });
 
     vkDestroyDevice(m_device, nullptr);
-    vkDestroySurfaceKHR(m_instance, m_surface, nullptr);
-    vkDestroyInstance(m_instance, nullptr);
-
+    vkDestroySurfaceKHR(m_vulkanInstance->get(), m_surface, nullptr);
 }
 
 VkDevice Device::getDevice() const {
     return m_device;
 }
 
-void Device::createVulkanInstance() {
-    if(m_isValidationEnabled && !checkValidationLayersSupport()) {
-        throw std::runtime_error("validation layers requested but not available");
-    }
-
-    #ifndef NDEBUG
-        showAllSupportedExtensions();
-    #endif
-
-    // optional
-    VkApplicationInfo appInfo{};
-    appInfo.sType              = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-    appInfo.pNext              = nullptr;
-    appInfo.pApplicationName   = "Vulkan learning";
-    appInfo.pEngineName        = nullptr;
-    appInfo.apiVersion         = VK_API_VERSION_1_0;
-    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.engineVersion      = VK_MAKE_VERSION(1, 0, 0);
-
-    const auto extensions { getRequiredInstanceExtensions() };
-
-    VkInstanceCreateInfo instanceInfo{};
-    instanceInfo.sType                   = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-    instanceInfo.pApplicationInfo        = &appInfo;
-    instanceInfo.enabledExtensionCount   = static_cast<uint32_t>(extensions.size());
-    instanceInfo.ppEnabledExtensionNames = extensions.data();
-
-    VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo{};
-    if(m_isValidationEnabled) {
-        instanceInfo.enabledLayerCount   = static_cast<uint32_t>(std::size(m_validationLayers));
-        instanceInfo.ppEnabledLayerNames = m_validationLayers.data();
-
-        populateDebugMessengerCreateInfo(debugCreateInfo);
-        instanceInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT*)&debugCreateInfo;
-    }
-    else {
-        instanceInfo.enabledLayerCount = 0u;
-        instanceInfo.pNext             = nullptr;
-    }
-
-    if(vkCreateInstance(&instanceInfo, nullptr, &m_instance) != VK_SUCCESS) {
-        throw std::runtime_error("failed to create vulkan instance");
-    }
-}
-
-void Device::setupDebugMessenger() {
-    if (!m_isValidationEnabled) return;
-
-    VkDebugUtilsMessengerCreateInfoEXT createInfo{};
-    populateDebugMessengerCreateInfo(createInfo);
-
-    if (getDebugUtilsMessengerEXT(m_instance, &createInfo, nullptr, &m_debugMessenger) != VK_SUCCESS) {
-        throw std::runtime_error("failed to set up debug messenger");
-    }
-}
-
-std::vector<const char*> Device::getRequiredInstanceExtensions() const {
-    uint32_t glfwExtensionCount{};
-    const char **glfwExtensions { glfwGetRequiredInstanceExtensions(&glfwExtensionCount) };
-
-    std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
-
-    if(m_isValidationEnabled) {
-        extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
-    }
-
-    return extensions;
-}
-
-void Device::showAllSupportedExtensions() const {
-    uint32_t extensionCount{};
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-    std::vector<VkExtensionProperties> extensions(extensionCount);
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
-
-    std::cout << "all available vulkan instance extensions: \n";
-    for(const auto& extension : extensions) {
-        std::cout << '\t' << extension.extensionName << '\n';
-    }
-}
-
-bool Device::checkValidationLayersSupport() const {
-    uint32_t layersCount{};
-    vkEnumerateInstanceLayerProperties(&layersCount, nullptr);
-
-    std::vector<VkLayerProperties> allLayers(layersCount);
-    vkEnumerateInstanceLayerProperties(&layersCount, allLayers.data());
-
-    size_t coveredLayers{};
-    for(const auto& layerName : m_validationLayers) {
-        for(const auto& layer : allLayers) {
-            if(strcmp(layerName, layer.layerName) == 0) {
-                coveredLayers++;
-            }
-        }
-    }
-
-    return coveredLayers == std::size(m_validationLayers);
-}
-
-VKAPI_ATTR VkBool32 VKAPI_CALL Device::debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
-                                                     VkDebugUtilsMessageTypeFlagsEXT messageType,
-                                                     const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
-                                                     void* pUserData) {
-    std::cerr << "validation layer: " << pCallbackData->pMessage << '\n';
-    return VK_FALSE;
-}
-
-VkResult Device::getDebugUtilsMessengerEXT(VkInstance instance, 
-                                           const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
-                                           const VkAllocationCallbacks* pAllocator,
-                                           VkDebugUtilsMessengerEXT* pDebugMessenger) const {
-    auto func { (PFN_vkCreateDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT") };
-
-    if (func != nullptr) {
-        return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
-    }
-    return VK_ERROR_EXTENSION_NOT_PRESENT;
-}
-
-void Device::destroyDebugUtilsMessengerEXT(VkInstance instance,
-                                           VkDebugUtilsMessengerEXT debugMessenger,
-                                           const VkAllocationCallbacks* pAllocator) const {
-    auto func { (PFN_vkDestroyDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT") };
-
-    if (func != nullptr) {
-        func(instance, debugMessenger, pAllocator);
-    }
-}
-
-void Device::populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo) const {
-    createInfo                 = {};
-    createInfo.sType           = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
-    createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
-    createInfo.messageType     = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
-    createInfo.pfnUserCallback = debugCallback;
-}
-
 void Device::pickPhysicalDevice() {
     uint32_t deviceCount{};
-    vkEnumeratePhysicalDevices(m_instance, &deviceCount, nullptr);
+    vkEnumeratePhysicalDevices(m_vulkanInstance->get(), &deviceCount, nullptr);
 
     if(deviceCount == 0u) {
         throw std::runtime_error("failed to find GPU with Vulkan support");
     }
 
     std::vector<VkPhysicalDevice> devices(deviceCount);
-    vkEnumeratePhysicalDevices(m_instance, &deviceCount, devices.data());
+    vkEnumeratePhysicalDevices(m_vulkanInstance->get(), &deviceCount, devices.data());
 
     std::multimap<uint32_t, VkPhysicalDevice> candidates{};
     
@@ -317,7 +173,7 @@ void Device::createLogicalDevice() {
 }
 
 void Device::createSurface() {
-    if(glfwCreateWindowSurface(m_instance, m_window->getWindowHandler(), nullptr, &m_surface) != VK_SUCCESS) {
+    if(glfwCreateWindowSurface(m_vulkanInstance->get(), m_window->getWindowHandler(), nullptr, &m_surface) != VK_SUCCESS) {
         throw std::runtime_error("failed to create window surface");
     }
 }
