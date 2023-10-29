b478fc15df0b9e0e6fc32ea18376660cba96b0af
apetor56 <lukten.github@gmail.com>
2023-10-29 02:07:36 +0200

    added pipeline

--- source/application.cpp
+++ source/application.cpp
@@ -9,7 +9,8 @@ Application::Application() : m_vulkanInstance { std::make_shared<VulkanInstance>
                              m_window{ std::make_shared<Window>(cfg::window::width, cfg::window::height, "example", m_vulkanInstance) },
                              m_physicalDevice { std::make_shared<PhysicalDevice>(m_vulkanInstance, m_window) },
                              m_logicalDevice { std::make_shared<LogicalDevice>(m_physicalDevice, m_window) },
-                             m_swapchain { std::make_shared<Swapchain>(m_physicalDevice, m_logicalDevice, m_window) }
+                             m_swapchain { std::make_shared<Swapchain>(m_physicalDevice, m_logicalDevice, m_window) },
+                             m_pipeline { std::make_shared<Pipeline>(m_logicalDevice) }
                             {}
 
 void Application::run() {
