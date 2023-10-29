7a40b5979826fc10f447fbd2a2501f12b03f8b97
apetor56 <lukten.github@gmail.com>
2023-10-29 02:07:02 +0200

    from scratch

--- source/pipeline.cpp
+++ source/pipeline.cpp
@@ -1,59 +1,31 @@
 #include "pipeline.hpp"
-#include <fstream>
-#include <stdexcept>
-#include <iostream>
+#include "config.hpp"
+
+#include <array>
+
+using stage_infos = std::array<VkPipelineShaderStageCreateInfo, 2u>;
 
 namespace VE {
-
-Pipeline::Pipeline(Device& device,
-                   std::string_view vertFilePath,
-                   std::string_view fragFilePath,
-                   const PipelineConfigInfo& configInfo) : m_device(device) {
-    createGraphicsPipeline(vertFilePath, fragFilePath, configInfo);
+Pipeline::Pipeline(std::shared_ptr<LogicalDevice> logicalDevice) :
+            m_vertexShader { cfg::shader::vertShaderBinaryPath, logicalDevice },
+            m_fragmentShader { cfg::shader::fragShaderBinaryPath, logicalDevice },
+            m_logicalDevice { logicalDevice } {
+    createPipeline();
 }
 
-std::vector<char> Pipeline::readFile(std::string_view filePath) {
-    std::ifstream file{filePath.data(), std::ios::ate | std::ios::binary};
+void Pipeline::createPipeline() {
+    VkPipelineShaderStageCreateInfo vertexStageCreateInfo{};
+    vertexStageCreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    vertexStageCreateInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+    vertexStageCreateInfo.module = m_vertexShader.getModule();
+    vertexStageCreateInfo.pName = "main";
 
-    if(file.is_open() == false) {
-        throw std::runtime_error("failed to open file: " + std::string{filePath.data()});
-    }
+    VkPipelineShaderStageCreateInfo fragmentStageCreateInfo{};
+    fragmentStageCreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    fragmentStageCreateInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+    fragmentStageCreateInfo.module = m_fragmentShader.getModule();
+    fragmentStageCreateInfo.pName = "main";
 
-    const size_t fileSize {static_cast<const size_t>(file.tellg())};
-    std::vector<char> buffer(fileSize);
-
-    file.seekg(0);
-    file.read(buffer.data(), fileSize);
-
-    file.close();
-    return buffer;
+    stage_infos shaderStages { vertexStageCreateInfo, fragmentStageCreateInfo };
 }
-
-void Pipeline::createGraphicsPipeline(std::string_view vertFilePath,
-                                      std::string_view fragFilePath,
-                                      const PipelineConfigInfo& configInfo) {
-    const auto vertCode { readFile(vertFilePath) };
-    const auto fragCode { readFile(fragFilePath) };
-
-    std::cout << "vertex shader binary code size: " << std::size(vertCode) << '\n'
-              << "fragment shader binary code size: " << std::size(fragCode) << '\n';
-}
-
-void Pipeline::createShaderModule(const std::vector<char> code, VkShaderModule *shaderModule) {
-    VkShaderModuleCreateInfo createInfo{};
-    createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
-    createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
-    createInfo.codeSize = std::size(code);
-
-    if(vkCreateShaderModule(m_device.getDevice(), &createInfo, nullptr, shaderModule) != VK_SUCCESS) {
-        throw std::runtime_error("failed to create shader module");
-    }
-}
-
-PipelineConfigInfo Pipeline::defaultPipelineConfigInfo(uint32_t width, uint32_t height) {
-    PipelineConfigInfo configInfo{};
-
-    return configInfo;
-}
-
 }
