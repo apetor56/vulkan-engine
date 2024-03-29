72c21a086fb2a2c0ae74ddb2e193b68947ae3890
apetor56 <lukten.github@gmail.com>
2024-03-29 22:38:18 +0100

    removed clang for now

--- setup.bat
+++ setup.bat
@@ -3,7 +3,6 @@
 echo Pass the number to select your toolset:
 echo 1) Visual Studio 19 2022: mvsc143,
 echo 2) MinGW:                 g++,
-echo 3) MinGW:                 clang++
 
 set /p userInput=
 
@@ -13,9 +12,5 @@ if %userInput% == 1 (
 )
 if %userInput% == 2 (
     cmake -S . -B build -DCMAKE_CXX_COMPILER=g++ --preset mingw-g++-debug
-    exit
-)
-if %userInput% == 3 (
-    cmake -S . -B build -DCMAKE_CXX_COMPILER=clang++ --preset mingw-clang++-debug
     exit
 )
