0f34ba079e9639ff2ffa75755e26d9515bf30bb5
apetor56 <lukten.github@gmail.com>
2024-04-21 01:11:33 +0200

    clang format + changed name convention

---
+++ source/Shader.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "LogicalDevice.hpp"
+
+#include <vulkan/vulkan.h>
+
+#include <string_view>
+#include <fstream>
+#include <vector>
+#include <memory>
+
+namespace ve {
+
+class Shader {
+public:
+    Shader( std::string_view shaderPath, std::shared_ptr< LogicalDevice > logicalDevice );
+    ~Shader();
+
+    VkShaderModule getModule() const;
+
+private:
+    VkShaderModule m_shaderModule;
+    std::shared_ptr< LogicalDevice > m_logicalDevice;
+
+    std::vector< char > readShaderBinary( std::string_view shaderPath ) const;
+    void createShaderModule( const std::vector< char >& shaderByteCode );
+};
+
+} // namespace ve
