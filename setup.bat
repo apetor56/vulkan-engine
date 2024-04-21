0089dcad4d6974b2553bcd057d852e1fe7294681
apetor56 <lukten.github@gmail.com>
2024-04-21 19:08:23 +0200

    added clang-cl, removed g++

--- setup.bat
+++ setup.bat
@@ -1,16 +1,10 @@
 @echo off
 
 echo Pass the number to select your toolset:
-echo 1) Visual Studio 19 2022: mvsc143,
-echo 2) MinGW:                 g++,
+echo 1) Visual Studio 17 2022: mvsc143,
+echo 2) Visual Studio 17 2022: clang-cl
 
 set /p userInput=
 
-if %userInput% == 1 (
-    cmake -S . --preset vs2022-mvsc-debug
-    exit
-)
-if %userInput% == 2 (
-    cmake -S . -DCMAKE_CXX_COMPILER=g++ --preset mingw-g++-debug
-    exit
-)
+if 		%userInput% == 1 ( cmake -S . --preset vs2022-mvsc-debug ) ^
+else if %userInput% == 2 ( cmake -S . --preset vs2022-clangcl-debug ) ^
