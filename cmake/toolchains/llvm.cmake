1a961ef9ee925ee7e9063c18fac3c1ffc9e67c37
apetor56 <lukten.github@gmail.com>
2024-04-21 19:13:18 +0200

    toolchain for LLVM clang++

---
+++ cmake/toolchains/llvm.cmake
@@ -0,0 +1,9 @@
+add_compile_options(
+    -Wall
+    -Wextra
+    -Wpedantic
+    -fsanitize=undefined
+)
+
+set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O3")
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -g")
